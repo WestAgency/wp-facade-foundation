288b04a605b004b89fcaf9923f7ebf4b73b603d5
Erik Landvall <erik@landvall.se>
2014-01-15 10:39:04 +0100

    Added a bootstrap supported walker for buttons

---
+++ inc/Facade/WP/Walker/NavigationMenu/BootstrapButtons.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @author Erik Landvall
+ */
+class Facade_WP_Walker_NavigationMenu_BootstrapButtons extends Walker_Nav_Menu
+{
+  /**
+   * @param string $output Passed by reference. Used to append additional
+   * content.
+   * @param object $item Menu item data object.
+   * @param int $depth Depth of menu item. Used for padding.
+   * @param object $args
+   */
+  function start_el( &$output, $item, $depth = 0, $args = array() )
+  {
+    $classes = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( array( 'cell', 'loose' )), $item, $args ) );
+    $classes = $classes ? ' class="' . esc_attr( $classes ) . '"' : '';
+
+    $output .= '<li' . $classes .'>';
+
+    $attributes  = ! empty( $item->attr_title )  ? ' title="'  . esc_attr( $item->attr_title ) . '"' : '';
+    $attributes .= ! empty( $item->target )      ? ' target="' . esc_attr( $item->target     ) . '"' : '';
+    $attributes .= ! empty( $item->xfn )         ? ' rel="'    . esc_attr( $item->xfn        ) . '"' : '';
+    $attributes .= ! empty( $item->url )         ? ' href="'   . esc_attr( $item->url        ) . '"' : '';
+    $attributes .= ' class="btn btn-primary"';
+
+    $item_output = $args->before
+      . '<a'. $attributes .'>'
+      . $args->link_before
+      . apply_filters( 'the_title', $item->title, $item->ID )
+      . $args->link_after
+      . '</a>'
+      . $args->after;
+
+    $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+  }
+}
