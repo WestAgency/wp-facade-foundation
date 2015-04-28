eec8e50055a9693e767ab43feba175811fd4b9a5
Erik Landvall <erik@landvall.se>
2015-04-28 18:16:45 +0200

    Altered how the cpt widhet functioned.. .. removed the unneccecery subclasses

--- inc/Facade/WP/Widget/CPT.php
+++ inc/Facade/WP/Widget/CPT.php
@@ -1,22 +1,27 @@
 <?php
 
-abstract class Facade_WP_Widget_CPT extends WP_Widget
+class Facade_WP_Widget_CPT extends WP_Widget
 {
-  abstract protected function getCpt();
-  abstract protected function getTitle();
-  abstract protected function getDescription();
+  protected
+    $cpt,
+    $title,
+    $description;
 
-  public function __construct()
+  public function __construct($cpt, $title, $description)
   {
+    $this->cpt         = $cpt;
+    $this->title       = $title;
+    $this->description = $description;
+
     $widget_options = array(
-      'classname'   => $this->getCpt() . '-widget',
-      'description' => $this->getDescription());
+      'classname'   => $this->cpt . '-widget',
+      'description' => $this->description);
 
     $control_options = array();
 
     parent::WP_Widget(
-      $this->getCpt() . 'widget',
-      $this->getTitle(),
+      $this->cpt . 'widget',
+      $this->title,
       $widget_options,
       $control_options);
   }
@@ -25,15 +30,15 @@ abstract class Facade_WP_Widget_CPT extends WP_Widget
   {
     $query = new WP_Query(
       array(
-        'p'         => $instance[$this->getCpt()],
-        'post_type' => $this->getCpt()));
+        'p'         => $instance[$this->cpt],
+        'post_type' => $this->cpt));
 
     if($query->have_posts())
     {
       echo $args['before_widget'];
 
       $query->the_post();
-      get_template_part('widget', $this->getCpt());
+      get_template_part('widget', $this->cpt);
       wp_reset_postdata();
 
       echo $args['after_widget'];
@@ -43,24 +48,24 @@ abstract class Facade_WP_Widget_CPT extends WP_Widget
   public function update( $new_instance, $old_instance )
   {
     $instance = $old_instance;
-    $instance[$this->getCpt()] = (int) $new_instance[$this->getCpt()];
-    $instance['title'] = get_the_title( $instance[$this->getCpt()] );
+    $instance[$this->cpt] = (int) $new_instance[$this->cpt];
+    $instance['title'] = get_the_title( $instance[$this->cpt] );
     return $instance;
   }
 
   public function form($instance)
   {
-    $instance = wp_parse_args((array) $instance, array('title' => '', $this->getCpt() => 0));
+    $instance = wp_parse_args((array) $instance, array('title' => '', $this->cpt => 0));
     echo '<input class="widefat" id="' . $this->get_field_id('title') . '" name="' . $this->get_field_name('title') . '" type="hidden" value="' . $instance['title'] . '" />'
         .'<p>'
-        .'<label for="' . $this->get_field_id($this->getCpt()) . '">' . __('Välj en post:');
+        .'<label for="' . $this->get_field_id($this->cpt) . '">' . __('Välj en post:');
 
-    $posts = get_posts('post_type=' . $this->getCpt() . '&post_status=publish&numberposts=-1');
+    $posts = get_posts('post_type=' . $this->cpt . '&post_status=publish&numberposts=-1');
     if(count($posts) > 0)
     {
-      echo '<select class="widefat" id="' . $this->get_field_id($this->getCpt()) . '" name="' . $this->get_field_name($this->getCpt()) . '">';
+      echo '<select class="widefat" id="' . $this->get_field_id($this->cpt) . '" name="' . $this->get_field_name($this->cpt) . '">';
       foreach($posts as $post)
-        echo '<option value="' . $post->ID . '"' . selected($post->ID, $instance[$this->getCpt()], false) . '>' . $post->post_title . '</option>';
+        echo '<option value="' . $post->ID . '"' . selected($post->ID, $instance[$this->cpt], false) . '>' . $post->post_title . '</option>';
       echo '</select>';
     }
     else
